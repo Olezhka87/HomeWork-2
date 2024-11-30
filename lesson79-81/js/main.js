264a990a4f2b6f16ac99b458a8962408f09338a7
Olezhka87 <125438310+Olezhka87@users.noreply.github.com>
2024-11-30 09:27:25 +0300

    Add files via upload

--- lesson79-81/js/main.js
+++ lesson79-81/js/main.js
@@ -2,20 +2,20 @@
 
                                    // // 79. Условия // //
 
-let age = 17
+// let age = 17
 
-if(age >= 18) {
-    console.log('Вы совершеннолетний');
-} else(age < 18)
- {
-    console.log('Вам нет 18 лет');
-}
+// if(age >= 18) {
+//     console.log('Вы совершеннолетний');
+// } else(age < 18)
+//  {
+//     console.log('Вам нет 18 лет');
+// }
 
-// ---- тернарный оператор js
+// // ---- тернарный оператор js
 
-const age1 = 20;
-const ageResual = age1 >= 19 ? ('Вы совершеннолетний') : ('Вам нет 18 лет');
-console.log(ageResual);
+// const age1 = 20;
+// const ageResual = age1 >= 19 ? ('Вы совершеннолетний') : ('Вам нет 18 лет');
+// console.log(ageResual);
 
 
 
@@ -27,66 +27,53 @@ console.log(ageResual);
 
 // 1) Выведите в консоль четные числа от 2 до 12, используйте цикл for и условие.
 
-for(let a = 2; a <= 12; a = a + 2) {
-    console.log(a);
-}
+// for(let a = 2; a <= 12; a = a + 2) {
+//     console.log(a);
+// }
 
-// 2) При помощи цикла for выведите в консоль числа от 10 до 1, в обратном порядке.
+// // 2) При помощи цикла for выведите в консоль числа от 10 до 1, в обратном порядке.
 
-for(let b = 10; b >= 1; b = b - 1) {
-    console.log(b)
-}
+// for(let b = 10; b >= 1; b = b - 1) {
+//     console.log(b)
+// }
 
 // 3) Перепишите 1 и 2 цикл используя цикл while.
 
-let ab = 0
-while(ab < 12) {
-    ab = ab + 2;
-    console.log(ab);
+// let ab = 0
+// while(ab < 12) {
+//     ab = ab + 2;
+//     console.log(ab);
 
-}
+// }
 
 // ---------------------------------------------------------
 
-let bb = 11
-while(bb > 1) {
+let bb = 10
+while(bb >= 1) {
     bb = bb - 1;
-    console.log(bb);
+    console.log(bb + 1);
 }
-
-
-
-
-
 
                                   // 81. Объектные типы данных. Массивы // //
 
 // 1) Создайте произвольный массив чисел. Используя цикл for обойдите этот массив и выведите в консоль только четные числа.
 // for (инициализация; условие; итерация) { //тело цикла}
 
-let numbers = [12,34,44,45,57]
+// let numbers = [12,34,44,45,57]
 
-for (let abc = 0; abc < numbers.length; abc++) {
-    if (numbers[abc] % 2 == 0)
-    console.log(numbers[abc]);
-}
+// for (let abc = 0; abc < numbers.length; abc++) {
+//     if (numbers[abc] % 2 == 0)
+//     console.log(numbers[abc]);
+// }
 
 
 // 2) Создайте массив из всех цветов радуги. Используя цикл for обойдите этот массив и выведите в консоль все цвета радуги, но в обратном порядке, 
 // p.s. обратите внимание на индексы массивов и стартовую позицию для i.
 
-// красный: #e81416; 2
-// оранжевый: #ffa500; 2
-// жёлтый: #faeb36; 2
-// зелёный: #79c314; 2
-// синий: #487de7; 2
-// индиго: #4b369d; 2
-// фиолетовый: #70369d.
 
 
-let colorRaduga = ['#e81416', '#ffa500', '#faeb36', '#79c314', '#487de7', '#4b369d', '#70369d']
-
-for (let color = 0; color < colorRaduga.length; color++) {
-    colorRaduga.reverse();
-    console.log(colorRaduga[color])
+let colorRaduga = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'] 
+for (let color = colorRaduga.length - 1; color >= 0; color--) { 
+    console.log(colorRaduga[color]);
 }
+
